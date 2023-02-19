6c2929c2f338b7448f0018c11689e74cf4476446
Amós Lima <social.amos@gmail.com>
2023-02-19 01:11:50 -0300

    add cycles interface, states

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,6 +15,7 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmaunt: zod
@@ -30,8 +31,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmaunt: number
+}
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -39,8 +48,20 @@ export function Home() {
     },
   })
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      task: data.task,
+      minutesAmaunt: data.minutesAmaunt,
+    }
+
+    setCycles((cycles) => [...cycles, newCycle])
+    setActiveCycleId(newCycle.id)
+    reset()
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
   const task = watch('task')
 
   return (
